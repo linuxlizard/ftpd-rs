fc683b4a6e6aaf4e34e06dec2b6681c2f272fcca
David Poole <testcluster@gmail.com>
2024-05-12 07:11:13 -0700

    baby steps
    - add my own readline() fn so can read+write the socket
    - start of actual ftp protocol support

--- src/main.rs
+++ src/main.rs
@@ -4,38 +4,53 @@ use std::io::{prelude::*};
 use std::net::{TcpListener, TcpStream};
 
 //const FTP_PORT:u16 = 2121;
+const HELLO : &[u8] = b"220 Hello from a silly ftp server..\r\n";
+const USERNAME_OK : &[u8] = b"331 User name okay, need password.\r\n";
+const PASSWORD_OK : &[u8] = b"230 User logged in, proceed.\r\n";
 
 fn readline(stream: &mut TcpStream) -> io::Result<String>
 {
-    let mut buf: [u8; 1] = [0;1];
+    let mut s = String::new();
 
     loop {
+        let mut b: [u8; 1] = [0];
 
-        let read_result = stream.read(&mut buf);
-        match read_result {
-            Ok(size) => {
-                println!("read size={} char={:#x}", size, buf[0]);
-                if size == 0 { break; }
-            },
-            Err(_err) => { panic!("failed to read"); }
+        stream.read_exact(&mut b)?;
+        println!("read b={:?}", b);
+        if b[0] == 0x0a {
+            break;
         }
+        s.push(b[0] as char);
+//        match read_result {
+//            Ok(()) => {
+//                println!("read b={:?}", b);
+//            },
+//            Err(err) => { panic!("failed to read"); }
+//        }
     }
 
-    Ok("todo".to_string())
+    Ok(s)
 }
 
 fn handle_connection(mut stream: TcpStream) 
 {
-    let _ = stream.write(b"username: ");
+    let _ = stream.write(HELLO);
 
     let username = readline(&mut stream).unwrap();
     println!("username={:?}", username);
+    let _ = stream.write(USERNAME_OK);
 
-    let _ = stream.write(b"password: ");
+//    let _ = stream.write(b"password: ");
     let password = readline(&mut stream).unwrap();
+    let _ = stream.write(PASSWORD_OK);
     println!("password={:?}", password);
 
     println!("connection done");
+    loop {
+        let cmd = readline(&mut stream).unwrap();
+        println!("command={:?}", cmd);
+    }
+
 }
 
 fn main() {
