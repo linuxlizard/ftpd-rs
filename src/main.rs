90d3d63acdc845d0633b3c41a1e715d174904c19
David Poole <testcluster@gmail.com>
2024-06-08 15:59:44 -0700

    start on LIST command; add return type to command handlers; refactor data port into a struct method

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,13 @@ use std::collections::HashMap;
 use std::cell::RefCell;
 
 //const FTP_PORT:u16 = 2121;
-const HELLO : &[u8] = b"220 Hello from a silly ftp server..\r\n";
-const USERNAME_OK : &[u8] = b"331 User name okay, need password.\r\n";
-const PASSWORD_OK : &[u8] = b"230 User logged in, proceed.\r\n";
-const BYE : &[u8] = b"200 bye!\r\n";
-const SYST_RESPONSE : &[u8] = b"215 UNIX Type: L8\r\n";
+const MSG_HELLO: &[u8] = b"220 Hello from a silly ftp server..\r\n";
+const MSG_USERNAME_OK: &[u8] = b"331 User name okay, need password.\r\n";
+const MSG_PASSWORD_OK: &[u8] = b"230 User logged in, proceed.\r\n";
+const MSG_BYE: &[u8] = b"200 bye!\r\n";
+const MSG_SYST_RESPONSE: &[u8] = b"215 UNIX Type: L8\r\n";
+const MSG_CLOSING : &[u8] = b"226 Closing data connection.\r\n";
+const MSG_DATA_TRANSFER_STARTING :&[u8] = b"125 Data connection already open; transfer starting.\r\n";
 
 const CR:u8 = b'\r';
 const LF:u8 = b'\n';
@@ -24,7 +26,7 @@ fn readline(stream: &mut TcpStream) -> io::Result<String>
         let mut b: [u8; 1] = [0];
 
         stream.read_exact(&mut b)?;
-        println!("read b={:?}", b);
+        println!("read b={:?} c={:?}", b, b[0] as char);
         if b[0] == CR {
             continue;
         }
@@ -68,24 +70,53 @@ impl State {
             data: None
         }
     }
+
+    pub fn open_data_port(&mut self ) -> io::Result<usize>
+    {
+        match self.data {
+            None => {
+                let bindto = self.ctrl.borrow_mut().local_addr().unwrap().ip().to_string() + ":0";
+                let listener = TcpListener::bind(bindto).unwrap();
+                self.data = Some(RefCell::new(listener));
+                // TODO handle ASCII mode
+//                write_msg(b"150 Open BINARY mode data connection.\r\n", self)
+                Ok(0)
+            },
+            Some(_) => {
+                write_msg(MSG_DATA_TRANSFER_STARTING, self)
+            }
+        }
+    }
+
+    pub fn close_data_port(&mut self)
+    {
+        self.data = None;
+    }
 }
-fn handle_syst(_args: &str, state: &mut State)
+
+fn write_msg(msg: &[u8], state: &mut State) -> io::Result<usize>
+{
+    state.ctrl.borrow_mut().write(msg)
+}
+
+fn handle_syst(_args: &str, state: &mut State) -> io::Result<usize>
 {
     println!("handle_syst");
-    let _ = state.ctrl.borrow_mut().write(SYST_RESPONSE);
+    write_msg(MSG_SYST_RESPONSE, state)
 }
 
-fn handle_pasv( _args: &str, state: &mut State)
+fn handle_pasv( _args: &str, state: &mut State) -> io::Result<usize>
 {
     println!("handle_pasv");
-
+/*
     let bindto = state.ctrl.borrow_mut().local_addr().unwrap().ip().to_string() + ":0";
 
     let listener = TcpListener::bind(bindto).unwrap();
 
-//    let socket = listener.local_addr().unwrap();
-
     state.data = Some(RefCell::new(listener));
+*/
+    state.close_data_port();
+    state.open_data_port()?;
 
     // local_addr to find the IP address of the socket
     let socket = state.data.as_ref().unwrap().borrow().local_addr().unwrap();
@@ -94,26 +125,27 @@ fn handle_pasv( _args: &str, state: &mut State)
                            socket.port() / 256,
                            socket.port() % 256
                            );
-    let _ = state.ctrl.borrow_mut().write_all(response.as_bytes());
+    write_msg(response.as_bytes(), state)
 }
 
-fn handle_type( args: &str, state: &mut State)
+fn handle_type( args: &str, state: &mut State) -> io::Result<usize>
 {
     // created by AI assistant :-O
     let args = args.trim();
     if args == "A" {
-        state.ctrl.borrow_mut().write_all(b"200 Type set to ASCII.\r\n").unwrap();
+        write_msg(b"200 Type set to ASCII.\r\n", state)
     } else if args == "I" {
-        state.ctrl.borrow_mut().write_all(b"200 Type set to Binary.\r\n").unwrap();
+        write_msg(b"200 Type set to Binary.\r\n", state)
     } else {
-        state.ctrl.borrow_mut().write_all(b"504 Command not implemented for that parameter.\r\n").unwrap();
+        write_msg(b"504 Command not implemented for that parameter.\r\n", state)
     }
 }
-fn handle_stor(args: &str, state: &mut State)
+fn handle_stor(args: &str, state: &mut State) -> io::Result<usize>
 {
-    state.ctrl.borrow_mut().write_all(b"125 Data connection already open; transfer starting.").unwrap();
-
     let mut file = std::fs::File::create(args).unwrap();
+
+//    let _ = state.open_data_port();
+    write_msg(b"150 Open BINARY mode data connection.\r\n", state)?;
 
     let accepting = state.data.as_ref().unwrap().borrow_mut().accept();
 
@@ -123,12 +155,33 @@ fn handle_stor(args: &str, state: &mut State)
     let result = std::io::copy(&mut data_stream.0, &mut file).unwrap();
     println!("copied {} bytes", result);
 
-    let _ = state.ctrl.borrow_mut().write_all(b"226 Closing data connection.");
+    // close the data socket
+    state.close_data_port();
+
+    write_msg(MSG_CLOSING, state)
+}
+fn handle_list(_args: &str, state: &mut State) -> io::Result<usize>
+{
+    println!("handle_list");
+
+    state.open_data_port()?;
+
+    let accepting = state.data.as_ref().unwrap().borrow_mut().accept();
+    let mut data_stream = accepting.unwrap();
+    println!("connection from {}", data_stream.1.to_string());
+
+    let _ = data_stream.0.write_all(b"stuff stuff stuff\r\n\r\n");
+
+    state.close_data_port();
+
+    println!("handle_list done");
+
+    write_msg(MSG_CLOSING, state)
 }
 
 fn handle_connection(mut stream: TcpStream) 
 {
-    let _ = stream.write(HELLO);
+    let _ = stream.write(MSG_HELLO);
 
     let s = readline(&mut stream).unwrap();
     let (cmd, arg) = parse_command(&s).unwrap();
@@ -136,20 +189,21 @@ fn handle_connection(mut stream: TcpStream)
         return;
     }
     println!("username={:?}", arg);
-    let _ = stream.write(USERNAME_OK);
+    let _ = stream.write(MSG_USERNAME_OK);
 
 //    let _ = stream.write(b"password: ");
     let s = readline(&mut stream).unwrap();
     let (cmd, arg)  = parse_command(&s).unwrap();
     if cmd != "PASS" { return; }
-    let _ = stream.write(PASSWORD_OK);
+    let _ = stream.write(MSG_PASSWORD_OK);
     println!("password={:?}", arg);
 
-    let mut map: HashMap<String, fn(&str, &mut State)> = HashMap::new();
+    let mut map: HashMap<String, fn(&str, &mut State) -> io::Result<usize>> = HashMap::new();
     map.insert("SYST".to_string(), handle_syst);
     map.insert("PASV".to_string(), handle_pasv);
     map.insert("TYPE".to_string(), handle_type);
     map.insert("STOR".to_string(), handle_stor);
+    map.insert("LIST".to_string(), handle_list);
 
     let mut state = State::new(stream);
 
@@ -159,12 +213,12 @@ fn handle_connection(mut stream: TcpStream)
 
         println!("command={:?}", cmd);
         if cmd == "QUIT" {
-            let _ = state.ctrl.borrow_mut().write(BYE);
+            let _ = state.ctrl.borrow_mut().write(MSG_BYE);
             break;
         }
 
         if let Some(func) = map.get(&cmd) {
-            func(&arg, &mut state);
+            let _ = func(&arg, &mut state);
         }
     }
     println!("connection done");
